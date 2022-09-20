c49aac12a3d10fda7db7597194573a05e2a87128
Gabriel Almeida <gabrielalmeida250@outlook.com>
2022-09-20 18:02:41 -0300

    Trilha de Formatação Técnica - Spring Boot API Rest: Segurança da API, Cache e Monitoramento - Aula 02

--- SpringBootAlura/SpringBootAPIRest/forum/forum/src/main/java/br/com/alura/forum/controllers/TopicosController.java
+++ SpringBootAlura/SpringBootAPIRest/forum/forum/src/main/java/br/com/alura/forum/controllers/TopicosController.java
@@ -1,13 +1,16 @@
 package br.com.alura.forum.controllers;
 
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -38,12 +42,15 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 
 	@GetMapping
-	public List<TopicoDto> lista(String nomeCurso) {
+	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @PageableDefault(sort = "id", direction = Direction.DESC, page = 0, size = 10) Pageable pageable) {
+		
+		//Pageable pageable = PageRequest.of(pagina, qtd, Direction.DESC, ordenacao);
+		
 		if (nomeCurso == null) {
-			List<Topico> topicos = topicoRepository.findAll();
+			Page<Topico> topicos = topicoRepository.findAll(pageable);
 			return TopicoDto.converter(topicos);
 		} else {
-			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+			Page<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso, pageable);
 			return TopicoDto.converter(topicos);
 		}
 	}
